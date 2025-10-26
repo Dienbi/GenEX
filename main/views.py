2c61babbe7d29c54b52eb7f9457f3e91f18839f7
Dhia Borji <dhia.borji2001@gmail.com>
2025-10-26 14:59:46 +0100

    fixed paths

--- main/views.py
+++ main/views.py
@@ -77,7 +77,7 @@ def signin(request):
         # Redirect based on user type
         if request.user.is_superuser or request.user.user_type == 'admin':
             return redirect('users:backoffice_dashboard')
-        return redirect('main:dashboard')
+        return redirect('main:home')
     
     if request.method == 'POST':
         username = request.POST.get('username')
@@ -97,8 +97,8 @@ def signin(request):
             if user.is_superuser or user.user_type == 'admin':
                 return redirect('users:backoffice_dashboard')
             
-            # Redirect to next page if specified, otherwise dashboard
-            next_page = request.GET.get('next', 'main:dashboard')
+            # Redirect to next page if specified, otherwise home
+            next_page = request.GET.get('next', 'main:home')
             return redirect(next_page)
         else:
             messages.error(request, 'Invalid username or password!')
